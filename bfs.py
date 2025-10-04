a6f1b056e461d3c0a6b092d33d702424271e93ac
thuck22 <thuc.nguyenkhmtk22@hcmut.edu.vn>
2025-10-05 01:32:28 +0700

    feat: add bfs algo for player to push the ruby

--- bfs.py
+++ bfs.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
 
-file_name = "map_18"
+file_name = "map_1"
 df = pd.read_csv(f'tile_maps/{file_name}.csv',header=None, index_col=None )
 nf = df.to_numpy()
 
-des_pos = np.argwhere((nf == 4) | (nf == 5))
-ruby_pos = np.argwhere((nf == 3) | (nf == 5))
-player_pos = np.argwhere(nf == 2)
+des_pos = np.argwhere((nf == 4) | (nf == 5)).tolist()
+ruby_pos = np.argwhere((nf == 3) | (nf == 5)).tolist()
+player_pos = np.argwhere(nf == 2).tolist()[0]
 
-print(des_pos)
-print(ruby_pos)
+
+print(f'des:',des_pos)
+print(f'ruby',ruby_pos)
+print('player', player_pos)
 
 nf = np.where((nf != 0)&(nf != 1), 0, nf)
 df = pd.DataFrame(nf)
@@ -24,7 +26,7 @@ def can_move(matrix, x, y):
     horizontal = matrix[x][y-1] == matrix[x][y+1] == 0
     return vertical, horizontal
 
-def bfs(queue, road, visited):
+def bfs(queue, road, visited, destination):
     while True:
         x,y = queue.pop(0)
         vertical, horizontal = can_move(nf,x, y)
@@ -50,13 +52,67 @@ def bfs(queue, road, visited):
                 road[x+1,y] = [x,y]
                 visited.append(bottom)
                 queue.append(bottom)
-        if any(np.array_equal(row, [x,y]) for row in des_):
+        if [x,y] == destination:
             break
         if len(queue) == 0:
             return []
     return road
 
+def player_bfs(queue, road, visited, destination):
+    while True:
+        x, y = queue.pop(0)
+        left = [x,y-1]
+        right = [x, y+1]
+        up = [x-1, y]
+        bottom = [x+1,y]
+        
+        if left not in visited and nf[x][y-1] == 0:
+            visited.append(left)
+            road[x,y-1] = [x,y]
+            queue.append(left)
+        if right not in visited and nf[x][y+1] == 0:
+            road[x,y+1] = [x,y]
+            visited.append(right)
+            queue.append(right)
+        if up not in visited and nf[x-1][y] == 0:
+            road[x-1,y] = [x,y]
+            visited.append(up)
+            queue.append(up)
+        if bottom not in visited and nf[x+1][y] == 0:
+            road[x+1,y] = [x,y]
+            visited.append(bottom)
+            queue.append(bottom)
+        if [x,y] == destination:
+            break
+        if len(queue) == 0:
+            return []
+    return road
 
+def player_pos_to_push(move, next_move):
+    a = move[0]
+    b = move[1]
+    x = next_move[0]
+    y = next_move[1]
+    if a == x:
+        if b + 1 == y:
+            return [a, b -1], "right"
+        else:
+            return [a, b+ 1], "left"
+    else:
+        if a + 1 == x:
+            return [a-1, b], "up"
+        else: 
+            return [a+1, b], "down"
+
+def find_best_road(des, dic):
+    road = []
+    road.append([des[0],des[1]])
+    prev = dic[des[0],des[1]]
+    while prev != []:
+        road.append(prev)
+        prev = dic[prev[0],prev[1]]
+    road = road[::-1]
+    return road
 
 for ruby in ruby_pos:
     x,y = ruby
@@ -64,19 +120,29 @@ for ruby in ruby_pos:
     par = {}
     par[x,y] = []
     visited = [[x,y]]
-    par = bfs(queue,par,visited)
     
-    road = []
     for des in des_:
+        par = bfs(queue,par,visited, des)
         if par.get((des[0],des[1])) is not None:
             des_.remove([des[0],des[1]])
             
-            road.append([des[0],des[1]])
-            prev = par[des[0],des[1]]
-            while prev != []:
-                road.append(prev)
-                prev = par[prev[0],prev[1]]
+            
+            road = find_best_road(des,par)
             break
-    
-    road = road[::-1]
+            
     print(f'road from {road[0]} to {road[-1]}: {road}' )
+    
+    while len(road) > 1:
+        curr = road[0]
+        next = road[1]
+        road.pop(0)
+        prev, order = player_pos_to_push(curr, next)
+        x ,y = player_pos
+        q = [[x,y]]
+        r = {}
+        r[x,y] = []
+        v = [[x,y]]
+        dic_road = player_bfs(q, r, v, prev)
+        player_road = find_best_road(prev, dic_road)
+        player_pos = curr
+        print(player_road, order)
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from img_to_matrix import *
 if __name__ == "__main__":
-    matrix = img2matrix("map_18")
+    matrix = img2matrix("map_3")
